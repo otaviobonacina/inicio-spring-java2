14f8f8825e18c5b717c104b5e26bc4c366f1a86a
otaviobonacina <otavio.bernado@fatec.sp.gov.br>
2023-10-16 21:53:26 +0000

    comit

--- app/src/main/java/application/controller/IndexController.java
+++ app/src/main/java/application/controller/IndexController.java
@@ -2,12 +2,17 @@ package application.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
     @RequestMapping("/")
-    public String home() {
-        return "WEB-INF/home.jsp";
+    public String home(
+        Model visualiacao,
+        @RequestParam("nome")String n) {
+        visualiacao.addAttribute("mensagem", "olá " + n);
+        return "home";
     }
 }
 
